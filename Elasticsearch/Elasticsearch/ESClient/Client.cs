e1eec30d9fbc3de8d1713732e3d75b27b44fd6b5
Danny Burrows <burrows.danny@gmail.com>
2016-09-09 23:03:26 -0500

    Added json file for AWS credentials. Added elasticsearch client and aws credential helper.

---
+++ Elasticsearch/Elasticsearch/ESClient/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Elasticsearch.Net;
+using Elasticsearch.Net.Aws;
+using Nest;
+using Newtonsoft.Json;
+
+namespace Elasticsearch.ESClient
+{
+    internal class AWSCredentials
+    {
+        internal string Key { get; set; }
+        internal string Secret { get; set; }
+        internal string Region { get; set; }
+    }
+
+    public class Client
+    {
+        private ElasticClient _client;
+
+        public Client( )
+        {
+            AWSCredentials credentials;
+            using ( StreamReader jsonFile = File.OpenText( string.Format( @"{0}aws.config.json", AppDomain.CurrentDomain.BaseDirectory ) ) )
+            {
+                string json = jsonFile.ReadToEnd( );
+                credentials = JsonConvert.DeserializeObject< AWSCredentials >( json );
+
+                if (credentials == null) 
+                    throw new Exception("Whoops...");
+            }
+
+            var httpConnection = new AwsHttpConnection(new AwsSettings
+            {
+                AccessKey = credentials.Key,
+                SecretKey = credentials.Secret,
+                Region = credentials.Region
+            });
+
+
+            // create uri and connect to the service
+            var pool = new SingleNodeConnectionPool( new Uri( "http://search-slayingdragons-rjkkghkxvgnzkzzcm6b4savkme.us-west-2.es.amazonaws.com" ) );
+            var config = new ConnectionSettings( pool, httpConnection );
+            _client = new ElasticClient( config );
+        }
+    }
+}
---
+++ Elasticsearch/Elasticsearch/ESClient/IClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.ESClient
+{
+    public interface IClient
+    {
+    }
+}

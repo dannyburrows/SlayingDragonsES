f61d3f3579938ff084e9f09352691c47aa2dfe09
Danny Burrows <burrows.danny@gmail.com>
2016-09-09 20:45:12 -0500

    Scaffolded out simple solution with Swagger.

---
+++ Elasticsearch/Elasticsearch/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.Application;
+
+namespace Elasticsearch
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Swagger stuff
+            config.EnableSwagger( c => c.SingleApiVersion( "v1", "Slaying Dragons with Elasticsearch" ) ).EnableSwaggerUi( );
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}

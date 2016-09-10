8bfa591b1c7c4501ec2b617d2560a3cc843e6667
Danny Burrows <burrows.danny@gmail.com>
2016-09-10 02:16:47 -0500

    ES queries for quests by search text and date range.

--- Elasticsearch/Elasticsearch/ESClient/Client.cs
+++ Elasticsearch/Elasticsearch/ESClient/Client.cs
@@ -49,10 +49,99 @@ namespace Elasticsearch.ESClient
             _client = new ElasticClient( config );
         }
 
+        #region Quest
+
+        /// <summary>
+        /// Searches the quest index via standard query
+        /// </summary>
+        /// <param name="query">Search text</param>
+        /// <returns></returns>
+        public async Task< IEnumerable< Models.Quest > > SearchQueryViaQuery( string query )
+        {
+            IEnumerable< Models.Quest > result = null;
+
+            var response = await _client.SearchAsync< Models.Quest >( s => s.Index( "Quest" )
+                .Query( q =>
+                    q.MultiMatch( mm =>
+                        mm.Query( query )
+                            .Type( TextQueryType.BestFields )
+                            .Fields( f =>
+                                f.Fields( f2 => f2.Name, f2 => f2.Description )
+                            ) ) )
+                .Sort( s2 => s2.Descending( SortSpecialField.Score ) ) );
+
+            if ( response.Hits.Any( ) )
+            {
+                result = response.Hits.Select( h => h.Source );
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Searchs the quest index via a fuzzy query
+        /// </summary>
+        /// <param name="query">Search text</param>
+        /// <param name="fuzzy">Fuzziness index</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Models.Quest>> SearchQueryViaQueryFuzzy(string query, int fuzzy)
+        {
+            IEnumerable<Models.Quest> result = null;
+
+            var response = await _client.SearchAsync< Models.Quest >( s => s.Index( "Quest" )
+                .Query( q =>
+                    q.FunctionScore( fs => // define our own specific function to score
+                        fs.Query( q2 =>
+                            q2.MultiMatch( mm => // search on multiple fields
+                                mm.Query( query )
+                                    .Type( TextQueryType.BestFields )
+                                    .Fields( f =>
+                                        f.Field( f2 => f2.Name, 10.00 ) // Set the field and the boost
+                                            .Field( f2 => f2.Description, 2.00 ) // higher boost means higher score
+                                    ).Fuzziness( Fuzziness.EditDistance( fuzzy ) ) // controls amount of fuzzy
+                                ) ) ) )
+                .Sort( s2 => s2.Descending( SortSpecialField.Score ) ) );
+
+            if (response.Hits.Any())
+            {
+                result = response.Hits.Select(h => h.Source);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Searchs quests that start within the passed in time frames
+        /// </summary>
+        /// <param name="queryStart">Starting date</param>
+        /// <param name="queryEnd">Ending date</param>
+        /// <returns></returns>
+        public async Task< IEnumerable< Models.Quest > > FilterViaDateTime( DateTime queryStart, DateTime? queryEnd = null )
+        {
+            if ( queryEnd == null )
+                queryEnd = queryStart.AddDays( 7 );
+
+            IEnumerable< Models.Quest > result = null;
+            var response = await _client.SearchAsync< Models.Quest >( s => s.Index( "Quest" )
+                .Query( q =>
+                    q.DateRange( dr =>
+                        dr.Field( f => f.BeginDate ) // the field being search
+                            .GreaterThanOrEquals( queryStart )
+                            .LessThanOrEquals( queryEnd ) ) )
+                .Sort( so => so.Ascending( a => a.BeginDate ) ) ); // sort by soonest
+            if ( response.Hits.Any( ) )
+            {
+                result = response.Hits.Select( h => h.Source );
+            }
+            return result;
+        }
+        #endregion
+
+        #region Treasure
         /// <summary>
         /// Searches the treasures via standard query
         /// </summary>
-        /// <param name="query"></param>
+        /// <param name="query">Search text</param>
         /// <returns></returns>
         public async Task< IEnumerable< Models.Treasure > > SearchTreasureViaQuery( string query )
         {
@@ -65,7 +154,8 @@ namespace Elasticsearch.ESClient
                             .Type( TextQueryType.BestFields )
                             .Fields( f =>
                                 f.Fields( f2 => f2.Name, f2 => f2.Description, f2 => f2.Value ) // each field we want to search
-                            ) ) ) );
+                            ) ) )
+                .Sort( s2 => s2.Descending( SortSpecialField.Score ) ) ); // ensure best scores are bubbled to the top
 
             if ( response.Hits.Any( ) )
             {
@@ -78,7 +168,7 @@ namespace Elasticsearch.ESClient
         /// <summary>
         /// Searches the treasure index via fuzzy search, with an emphasis on name matching
         /// </summary>
-        /// <param name="query">String to search for</param>
+        /// <param name="query">Search text</param>
         /// <param name="fuzzy">Fuzziness index</param>
         /// <returns></returns>
         public async Task<IEnumerable<Models.Treasure>> SearchTreasureViaQueryFuzzy( string query, int fuzzy = 3 )
@@ -97,12 +187,14 @@ namespace Elasticsearch.ESClient
                                             .Field( f2 => f2.Description, 2.00 ) // higher boost means higher score
                                             .Field( f2 => f2.Value, 1.00 )
                                     ).Fuzziness( Fuzziness.EditDistance( fuzzy ) ) // controls amount of fuzzy
-                                ) ) ) ) );
+                                ) ) ) )
+                .Sort( s2 => s2.Descending( SortSpecialField.Score ) ) );
             if ( response.Hits.Any( ) )
             {
                 result = response.Hits.Select( h => h.Source );
             }
             return result;
         }
+        #endregion
     }
 }
